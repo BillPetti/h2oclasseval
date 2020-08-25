ecf8c8019f7c8b385528df95febedb7fa247ff4f
Bill Petti <billpetti@gmail.com>
2020-08-25 09:59:52 -0400

    pushing experimental h2o package
    R package for evaluating classification models built with h2o automl

---
+++ R/custom_h2o_confusion_metrics.R
@@ -0,0 +1,120 @@
+#' Custom classification metrics for h2o automl models
+#'
+#' @param model An existin h2o model object.
+#' @param newdata An h2o data object to apply the model object to. Typically a test set.
+#' @param threshold A range of threshold/cut-off values to evaluate.
+#'
+#' @import h2o purrr dplyr tibble
+#'
+#' @return A data.frame containing metrics for each threshold value provided.
+#'
+#' @description
+#'
+#' model: The model name
+#' threshold: Threshold value
+#' logloss: The logloss of the model (see \url{https://www.kaggle.com/dansbecker/what-is-log-loss} for more information)
+#' precision: True Positives / Predicted Positives
+#' recall: True Positives / All Positives
+#' neg_precision: Negative Prevision, True Negatives / Predicted Negatives
+#' specificity: True Negatives / All Negatives
+#' fall_out: 1 - \code{specificity}, used to plot ROC curves
+#' f1_score: F1 Score or Balanced Accuracy, the harmonic mean of \code{precision} and \code{recall}
+#' true_positives: Number of True Positives
+#' false_negatives: Number of False Negatives
+#' false_positives: Number of False Positives
+#' true_negatives: Number of True Negatives
+#'
+#' @export
+#'
+#' @examples \dontrun{custom_h2o_confusion_metrics(model = NA, newdata = NA, threshold = c(0,1)}
+
+custom_h2o_confusion_metrics <- function(model = NULL,
+                                         newdata = NULL,
+                                         threshold = c(.4,.6)) {
+
+  if(is.null(model) == TRUE) {
+
+    stop('Please provide an h2o model object.')
+  }
+
+  if(is.null(newdata) == TRUE) {
+
+    stop('Please provide an h2o data set.')
+  }
+
+  map_thresholds <- function(predictions,
+                             newdata,
+                             threshold) {
+
+    predictions_aug <- predictions %>%
+      as.data.frame() %>%
+      mutate(label = factor(ifelse(p1 > threshold, "1", "0"))) %>%
+      mutate(truth = ifelse(newdata$Class == 1, "1", "0")) %>%
+      mutate(truth = as.factor(truth))
+
+    alpha_confusion_matrix <- predictions_aug %>%
+      dplyr::group_by(label, truth) %>%
+      dplyr::count() %>%
+      dplyr::ungroup()
+
+    master_confusion_matrix <- tibble::tibble(label = as.character(c(0,0,1,1)),
+                                              truth = as.character(c(0,1,0,1)))
+
+    confusion_matrix <- master_confusion_matrix %>%
+      dplyr::left_join(alpha_confusion_matrix, by = c("label", "truth")) %>%
+      replace(is.na(.), 0)
+
+    true_positive_pred_positive <- as.numeric(confusion_matrix[4,3])
+    true_positive_pred_negative <- as.numeric(confusion_matrix[2,3])
+    true_negative_pred_positive <- as.numeric(confusion_matrix[3,3])
+    true_negative_pred_negative <- as.numeric(confusion_matrix[1,3])
+
+    precision <- as.numeric(true_positive_pred_positive / (true_negative_pred_positive+true_positive_pred_positive))
+    recall <- as.numeric(true_positive_pred_positive / (true_positive_pred_positive+true_positive_pred_negative))
+    neg_precision <- as.numeric(true_negative_pred_negative / (true_negative_pred_negative+true_positive_pred_negative))
+    specificity <- as.numeric(true_negative_pred_negative / (true_negative_pred_negative+true_negative_pred_positive))
+    fall_out <- 1-specificity
+    logloss <- MLmetrics::LogLoss(y_pred = predictions_aug$p1,
+                                  y_true = as.numeric(as.character(predictions_aug$truth)))
+    f1_score <- (2 * precision * recall) / (precision+recall)
+
+    payload <- tibble::tibble(model = model@model_id,
+                              threshold = threshold,
+                              logloss = logloss,
+                              precision = precision,
+                              recall = recall,
+                              neg_precision = neg_precision,
+                              specificity = specificity,
+                              fall_out = fall_out,
+                              f1_score = f1_score,
+                              true_positives = true_positive_pred_positive,
+                              false_negatives = true_positive_pred_negative,
+                              false_positives = true_negative_pred_positive,
+                              true_negatives = true_negative_pred_negative)
+
+    return(payload)
+  }
+
+  if(is.h2o(newdata) == FALSE) {
+
+    new_data <- h2o::as.h2o(newdata)
+
+  } else {
+
+    new_data <- newdata
+  }
+
+  predictions <- h2o::h2o.predict(object = h2o::h2o.loadModel(paste0(path_slug, model@model_id)),
+                                  newdata = new_data) %>%
+    as.data.frame()
+
+  newdata <- as.data.frame(newdata)
+
+  mapped_metrics <- purrr::map_df(.x = seq(threshold[1], threshold[2], .01),
+                                  ~map_thresholds(predictions = predictions,
+                                                  newdata = newdata,
+                                                  threshold = .x))
+
+  mapped_metrics
+}
+
---
+++ R/save_h2o_models_varimport.R
@@ -0,0 +1,95 @@
+#' Save h2o automl models and variable importance
+#'
+#' @param leaderboard A leaderboard object generated from \code{h2o::automl}.
+#' @param max_top_models Number of models to save and generate variable importance for. If NA, all
+#' models will be processed.
+#' @param path_slug A file path where the models should be saved.
+#' @param time_stamp If TRUE, a time stamp is constructed using \code{Sys.time}. User can also
+#' provide their own time stamp as a string e.g. '2020_08_01'.
+#'
+#' @return Function will return a list with an element for each model containing the model object
+#' and the variable importance of the model (if appropriate--not availabel for Ensemble and GLM). Individual
+#' models will be saved in the directory provided by the \code{path_slug} argument, and a list with all model
+#' objects and variable importane will also be saved to the \code{path_slug} directory.
+#'
+#' @import h2o purrr dplyr tibble
+#'
+#' @export
+#'
+#' @examples \dontrun{save_h2o_models_varimport(leaderboard, max_top_models = NA,
+#' path_slug, time_stamp = TRUE)}
+
+save_h2o_models_varimport <- function(leaderboard,
+                                      max_top_models = NA,
+                                      path_slug,
+                                      time_stamp = TRUE) {
+
+  if(dir.exists(path_slug) == FALSE) {
+
+    stop('The path_slug provided is either incorrect or does not exist. Operation stopped.')
+  }
+
+  loop_save_models <- function(model_from_leaderboard,
+                               path_slug) {
+
+    model <- h2o::h2o.getModel(model_from_leaderboard)
+
+    h2o::h2o.saveModel(object = model,
+                       force=TRUE,
+                       path = path_slug)
+  }
+
+  message(paste0('Saving models to ', path_slug))
+
+  leaderboard$model_id %>%
+    as.data.frame() %>%
+    {if (!is.na(max_top_models)) dplyr::slice(.data = ., 1:max_top_models)
+      else dplyr::slice(.data = ., 1:length(.$model_id))} %>%
+    dplyr::pull(model_id) %>%
+    purrr::map(~loop_save_models(., path = path_slug))
+
+  performance_data <- function(model_from_leaderboard,
+                               path_slug) {
+
+    model <- h2o::h2o.getModel(model_from_leaderboard)
+
+    var_import <- model@model$variable_importances %>%
+      as.data.frame() %>%
+      dplyr::mutate(model_id = model@model_id) %>%
+      dplyr::select(model_id, everything())
+
+    model_list <- list(model_object = model,
+                       variable_importance = var_import)
+
+    model_list
+  }
+
+  names_to_pull <- leaderboard$model_id %>%
+    as.data.frame() %>%
+    {if (!is.na(max_top_models)) slice(.data = ., 1:max_top_models)
+      else slice(.data = ., 1:length(.$model_id))} %>%
+    dplyr::pull(model_id)
+
+  payload <- names_to_pull %>%
+    purrr::map(~performance_data(model_from_leaderboard = .)) %>%
+    setNames(nm = names_to_pull)
+
+  file_time_stamp <- if (time_stamp == TRUE) {
+
+    file_time_stamp <- gsub('-| |:', '_', Sys.time())
+
+  } else {
+
+    file_time_stamp <- time_stamp
+
+    return(file_time_stamp)
+  }
+
+  message(paste0('Saving model and variable importance list to ', path_slug))
+
+  saveRDS(payload, file = paste0(path_slug, file_time_stamp, "_h2o_train_model_metrics.rds"))
+
+  return(payload)
+
+}
+
